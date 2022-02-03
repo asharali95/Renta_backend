2b51f713d84d55793aa51b2289c1500e4120defb
asharali95 <ashar.alia1999@gmail.com>
2022-02-03 16:26:22 +0500

    recent commit

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const carRouter = require("./routes/carRoutes");
 const authRouter = require("./routes/authRoutes");
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/cars", carRouter);
 
 module.exports = app;
